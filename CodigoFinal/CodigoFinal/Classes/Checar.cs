775b60e882539ccec5be4892ff780aaef22e0573
RedBrickBurrito <a01568010@gmail.com>
2019-05-16 11:43:05 -0600

    v_1.1

---
+++ CodigoFinal/CodigoFinal/Classes/Checar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoFinal.Classes
+{
+    class Checar
+    {
+
+
+        public static void checador(string name)
+        {
+            try
+            {
+                int letras = name.Count();
+                if (letras <= 2)
+                {
+                    throw new MinimoLetrasException();
+                }
+            }
+            catch (MinimoLetrasException)
+            {
+                Console.WriteLine("nombre/apellido no válido");
+            }
+        }
+
+        public class MinimoLetrasException : Exception
+        {
+            public MinimoLetrasException()
+               : base()
+            {
+            }
+        }
+    }
+}
---
+++ CodigoFinal/CodigoFinal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodigoFinal.Classes;
+
+namespace CodigoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el nombre: ");
+            string name = Console.ReadLine();
+            Checar.checador(name);
+            Console.WriteLine("Ingresa tu apellido: ");
+            string apellido = Console.ReadLine();
+            Checar.checador(apellido);
+
+        }
+    }
+}
